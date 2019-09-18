efafcb89ee018ee76ea21e7550e3467a24f1a6e5
isiahturner <54381776+isiahturner@users.noreply.github.com>
2019-09-18 00:22:16 -0400

    Less Dumb Attempt

--- SolutionsAssignment3/Assignment3.m
+++ SolutionsAssignment3/Assignment3.m
@@ -17,8 +17,10 @@ prompt= input('Are you ready to play some sweet ass Tic-Tac-Toe?(Yes or No): ','
 
 %Showing the gameboard 
 if strcmpi('yes',prompt)
-    gameBoard= zeros(3,3)
-   
+    gameBoard= {1,2,3;4,5,6;7,8,9}
+
+    %delete cell arrays https://www.mathworks.com/help/matlab/matlab_prog/delete-data-from-a-cell-array.html
+  
 elseif strcmpi('no',prompt)
     disp('See ya loser')
     return
@@ -27,28 +29,40 @@ end
 
 %Player and CPU move code
 playerMove= input('Pick a spot 1-9: ');
+
     switch (playerMove)
         case 1
-            gameBoard(1,1)= 1
+            gameBoard{1,1}= 'X'
         case 2
-            gameBoard(1,2)= 1
+            gameBoard{1,2}= 'X'
         case 3
-            gameBoard(1,3)= 1
+            gameBoard{1,3}= 'X'
         case 4
-            gameBoard(2,1)= 1
+            gameBoard{2,1}= 'X'
         case 5
-            gameBoard(2,2)= 1
+            gameBoard{2,2}= 'X'
         case 6
-            gameBoard(2,3)= 1
+            gameBoard{2,3}= 'X'
         case 7
-            gameBoard(3,1)= 1
+            gameBoard{3,1}= 'X'
         case 8
-            gameBoard(3,2)= 1
+            gameBoard(3,2)= 'X'
         case 9
-            gameBoard(3,3)= 1
-           
-          disp(gameBoard)
-    end     
+            gameBoard{3,3}= 'X'
+    end
+    
+%Computer Move1 (change to if playerMove=n ????)
+if gameBoard{1,1}=='X' | gameBoard{1,3}=='X' | gameBoard{3,1}=='X' | gameBoard{3,3}=='X'
+    gameBoard{2,2}= 'O'
+else
+    gameBoard{1,3}='O'
+end
+
+    
+    
+    
+
+         
             
 
                
@@ -58,7 +72,6 @@ playerMove= input('Pick a spot 1-9: ');
 
 
 
-humanMove=
 
 
 
